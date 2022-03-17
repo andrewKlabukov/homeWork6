d11805696723b4a269619379711bcb6826be104a
Andrew Klabukov <Andrew.klabukov@yandex.ru>
2022-03-17 15:28:56 +0400

    change js file

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+class Car {
+  constructor(brand, model, yearOfManufacturing, maxSpeed, maxFuelVolume, fuelConsumption, currentFuelVolume, isStarted, mileage) {
+    this.brand = brand;
+    this.model = model;
+    this.yearOfManufacturing = yearOfManufacturing;
+    this.maxSpeed = maxSpeed;
+    this.maxFuelVolume = maxFuelVolume;
+    this.fuelConsumption = fuelConsumption;
+    this.currentFuelVolume = currentFuelVolume;
+    this.isStarted = isStarted;
+    this.mileage = mileage;
+  }
 
+  start() {
+    if (this.isStarted) {
+      
+    }
+  }
+
+  shutDownEngine() {
+    if (!this.isStarted) {
+      
+    }
+  }
+
+  fillUpGasTank() {
+
+  }
+
+  drive() {
+
+  }
+}
